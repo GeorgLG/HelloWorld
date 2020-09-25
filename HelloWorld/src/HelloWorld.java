74fa49695ac55bb16bbf6c848ccf0fe4f250a869
GeorgSchamber <GeorgSchamber@0-208-15>
2020-09-25 12:00:06 +0200

    FizzBuzz

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,9 +1,25 @@
 
 public class HelloWorld {
-
+static int counter =0;
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		
+		while(counter<100) {
+			counter++;
+			if(counter%3==0 && counter%5==0) {
+				System.out.println("FizzBuzz");}
+				else {
+					if(counter%3==0 ) {
+						System.out.println("Fizz");}
+						else {
+							if(counter%5==0) {
+								System.out.println("Buzz");
+							}
+								else{System.out.println(counter);}
+							}
+			}
+		}
 	}
-
 }
+	
+
+
